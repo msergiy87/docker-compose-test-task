46c8262ee257b57e9e9c5be8b47707b159c8e0a2
Sergii Melnyk <sergii.melnyk@mev.com>
2018-04-06 16:20:07 +0300

    second commit

--- Vagrantfile
+++ Vagrantfile
@@ -2,15 +2,15 @@ Vagrant.configure("2") do |config|
 
   config.vm.box = "ubuntu/xenial64"
   config.ssh.forward_agent = true
-#  config.vm.network "private_network", type: "dhcp"
+  config.vm.network "private_network", type: "dhcp"
 
   config.vm.provider "virtualbox" do |vb|
-    vb.name = "test"
+    vb.name = "test-docker"
     #vb.cpus = "2"
     vb.memory = "1024"
   end
 
-#  config.vm.provision :shell, path: "bootstrap.sh"
+  config.vm.provision :shell, path: "bootstrap.sh"
 
   config.vm.network :forwarded_port, guest: 80, host: 80
   config.vm.network :forwarded_port, guest: 8080, host: 8080
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,44 +1,41 @@
 #!/usr/bin/env bash
 
-set -x
+#set -x
 sudo true
 
 # list available updates
-#sudo apt-get update
+sudo apt-get update
 
 # upgrade the current packages
-#sudo apt-get upgrade -y
-
-# Install git
-#sudo apt-get install git
+sudo apt-get upgrade -y
 
 # Install Docker
-#sudo apt-get install apt-transport-https ca-certificates
-#sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
-#sudo sh -c 'echo "deb https://apt.dockerproject.org/repo ubuntu-xenial main" > /etc/apt/sources.list.d/docker.list'
-#sudo apt-get update
-#sudo apt-get purge lxc-docker
-#sudo apt-cache policy docker-engine
-#sudo apt-get install -y linux-image-extra-$(uname -r)
-#sudo apt-get install -y docker-engine
-#sudo apt-get install -y python-pip
-#sudo service docker start
-#sudo systemctl enable docker
-#sudo groupadd docker
-#sudo usermod -aG docker $(whoami)
+sudo apt-get install apt-transport-https ca-certificates
+sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+sudo sh -c 'echo "deb https://apt.dockerproject.org/repo ubuntu-xenial main" > /etc/apt/sources.list.d/docker.list'
+sudo apt-get update
+sudo apt-get purge lxc-docker
+sudo apt-cache policy docker-engine
+sudo apt-get install -y linux-image-extra-$(uname -r)
+sudo apt-get install -y docker-engine
+sudo apt-get install -y python-pip
+sudo service docker start
+sudo systemctl enable docker
+sudo groupadd docker
+sudo usermod -aG docker $(whoami)
 
-#echo "Docker engine installed"
+echo "Docker engine installed"
 
 # Install docker-compose
-#COMPOSE_VERSION=`git ls-remote https://github.com/docker/compose | grep refs/tags | grep -oP "[0-9]+\.[0-9]+\.[0-9]+$" | tail -n 1`
-#sudo sh -c "curl -L https://github.com/docker/compose/releases/download/${COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose"
-#sudo chmod +x /usr/local/bin/docker-compose
-#sudo sh -c "curl -L https://raw.githubusercontent.com/docker/compose/${COMPOSE_VERSION}/contrib/completion/bash/docker-compose > /etc/bash_completion.d/docker-compose"
+COMPOSE_VERSION=`git ls-remote https://github.com/docker/compose | grep refs/tags | grep -oP "[0-9]+\.[0-9]+\.[0-9]+$" | tail -n 1`
+sudo sh -c "curl -L https://github.com/docker/compose/releases/download/${COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose"
+sudo chmod +x /usr/local/bin/docker-compose
+sudo sh -c "curl -L https://raw.githubusercontent.com/docker/compose/${COMPOSE_VERSION}/contrib/completion/bash/docker-compose > /etc/bash_completion.d/docker-compose"
 
-#echo "Docker compose ${COMPOSE_VERSION} installed"
-
-# Pull repo
-git clone 
+echo "Docker compose ${COMPOSE_VERSION} installed"
 
 # Run docker-compose
-#docker-compose up -d
+cd /vagrant/app
+sudo docker-compose up -d
+
+echo "Run app completed"
